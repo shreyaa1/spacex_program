ba0469072bc8da0f09bbc05363471280f773b07d
shreyaa1 <shuklashreya801@gmail.com>
2020-09-19 15:55:26 +0530

    added server side code

---
+++ webpack.server.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+    entry: './src/server/index.ts',
+    target: 'node',
+    externals: [nodeExternals()],
+    output: {
+        path: path.resolve('server-build'),
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader'
+            },
+        ]
+    }
+};
